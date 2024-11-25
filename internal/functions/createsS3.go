0e1d530e8c8d88b5643dddbb83c3a69c36dfb1ed
OliverTeo288 <oliver_teo288@hotmail.com>
2024-11-25 17:28:19 +0800

    fix: added validation for S3 bucket creation

--- internal/functions/createsS3.go
+++ internal/functions/createsS3.go
@@ -2,14 +2,25 @@
 package functions
 
 import (
-	
+	"regexp"
+	"errors"
 	"raid/infra/internal/utils"
 	"raid/infra/internal/aws"
 )
 
 
 func CreateS3(profile, region string) error {
-		bucketName, err := utils.PromptInput("Enter the name of the S3 bucket:", nil, "test-dev-backend-tf-0000")
+
+		validate := func(input string) error {
+			// Regex to allow only alphanumeric characters and dashes, no spaces
+			re := regexp.MustCompile(`^[a-zA-Z0-9\-]+$`)
+			if !re.MatchString(input) {
+				return errors.New("role name must only contain alphanumeric characters or dashes, and no spaces")
+			}
+			return nil
+		}
+		
+		bucketName, err := utils.PromptInput("Enter the name of the S3 bucket:", validate, "test-dev-backend-tf-0000")
 		if err != nil {
 			return err
 		}
